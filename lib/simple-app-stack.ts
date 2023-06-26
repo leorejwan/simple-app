51b7643f97fdebb044904e0d6434cab316148d28
leo <leorejwan@gmail.com>
2023-06-26 15:42:26 -0300

    Created a s3 bucket

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,10 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+
+    const bucket = new Bucket(this, 'MySimpleAppBucket', {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
 
     // The code that defines your stack goes here
 
