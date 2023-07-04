4774ca4d7052fa150c643455cf2243d9a2bbe91e
leo <leorejwan@gmail.com>
2023-07-04 14:51:31 -0300

    Lambda get content from S3 bucket

--- api/get-photos/index.js
+++ api/get-photos/index.js
@@ -1,13 +1,28 @@
+const { S3 } = require("aws-sdk");
 
 const bucketName = process.env.PHOTO_BUCKET_NAME;
+
+function generateUrl(asset){
+
+}
+
 
 exports.handler = async function (event, context)
 {
     console.log("BuccketName: " + bucketName);
     var a = "HELLO"
+    try{
+        const { Contents: results } = await S3.listObjects({Bucket: bucketName}).promise();
+        const photos = await Promise.all(results.map(result => generateUrl(result)));
 
-    return {
-        statusCode: 200,
-        body: a,
-    };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(photos),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: error.message
+        };
+    }
 }
